624f7f356933f740708c55c834546d1d10812bcf
RenanBick <renanbickdev@gmail.com>
2024-03-12 00:37:23 -0300

    Atualizando componentes

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+
+}
